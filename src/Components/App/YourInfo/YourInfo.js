66ece3a51bcff96c3d016aa3902ae8801d2d8954
abelmuro <abelmuro93@gmail.com>
2023-02-09 11:58:52 -0800

    commit

--- src/Components/App/YourInfo/YourInfo.js
+++ src/Components/App/YourInfo/YourInfo.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import styles from "./styles.module.css";
 import images from './images';
 
 
 function YourInfo() {
+    const switchRef = useRef();
+    const monthly = useRef();
+    const yearly = useRef();
+    const [billing, setBilling] = useState("monthly");
+
+
+    const handleOptions = (e) => {
+        console.log(e.target);
+    }
+
+    const handleSwitch = () => {
+        const head = switchRef.current.querySelector("." + styles.head);
+        const currentHeadPosition = head.style.left;  
+        if(currentHeadPosition == ""){
+            head.style.left = "20px";
+            setBilling("yearly");
+        }          
+        else {
+            head.style.left = "";
+            setBilling("monthly");
+        }     
+   }
+
+   useEffect(() => {
+        if(billing == "yearly"){
+            monthly.current.id = "";
+            yearly.current.id = styles.currentPlan;
+        }
+    
+        else if(billing == "monthly"){
+            monthly.current.id = styles.currentPlan;
+            yearly.current.id = "";
+        }
+        
+   }, [billing])
+
+
+
+
     return(
         <section className={styles.yourInfo}>
             <h1 className={styles.title}>
@@ -12,42 +51,59 @@ function YourInfo() {
             <p className={styles.desc}>
                 You have the option of monthly or yearly billing.
             </p>
-            <div className={styles.options}>
+            <div className={styles.options} onClick={handleOptions}>
                 <img src={images["arcade"]} className={styles.optionsImage}/>
                 <div>
                     <h2 className={styles.optionsTitle}>
                         Arcade
                     </h2>
                     <p className={styles.optionsPrice}>
-                    $9/mo  
-                    </p>                    
+                        {billing == "monthly"? "$9/mo" : "$90/yr"}  
+                    </p>   
+                    {billing == "yearly" ? <p className={styles.freeMonths}>
+                        2 months free
+                    </p> : <></>}                   
                 </div>
             </div>
-            <div className={styles.options}>
+            <div className={styles.options} onClick={handleOptions}> 
                 <img src={images["advanced"]} className={styles.optionsImage}/>
                 <div>
                     <h2 className={styles.optionsTitle}>
                         Advanced
                     </h2>
                     <p className={styles.optionsPrice}>
-                    $12/mo  
-                    </p>                    
+                        {billing == "monthly" ? "$12/mo" : "$120/yr"}  
+                    </p> 
+                   {billing == "yearly" ? <p className={styles.freeMonths}>
+                        2 months free
+                    </p> : <></>}                       
                 </div>
             </div>
-            <div className={styles.options} >
+            <div className={styles.options} onClick={handleOptions}>
                 <img src={images["pro"]} className={styles.optionsImage}/>
                 <div>
                     <h2 className={styles.optionsTitle}>
                         Pro
                     </h2>
                     <p className={styles.optionsPrice}>
-                    $15/mo  
-                    </p>                    
+                        {billing == "monthly" ? "$15/mo" : "$150/yr"}  
+                    </p>  
+                    {billing == "yearly" ? <p className={styles.freeMonths}>
+                        2 months free
+                    </p> : <></>}                         
                 </div>
 
             </div>
             <div className={styles.switchContainer}>
-
+                <p className={styles.monthly} id={styles.currentPlan} ref={monthly}>
+                    Monthly
+                </p>
+                <div className={styles.switch} onClick={handleSwitch} ref={switchRef}>
+                    <div className={styles.head}></div>
+                </div>
+                <p className={styles.yearly} ref={yearly}>
+                    Yearly
+                </p>
             </div>
         </section>
     )
