69ce0a0c5739c2b7c5aa8c36a6af83ea0ddc9253
abelmuro <abelmuro93@gmail.com>
2023-02-09 13:58:21 -0800

    commit

--- src/Components/App/YourInfo/YourInfo.js
+++ src/Components/App/YourInfo/YourInfo.js
@@ -41,69 +41,75 @@ function YourInfo() {
    }, [billing])
 
 
-
-
     return(
         <section className={styles.yourInfo}>
-            <h1 className={styles.title}>
-                Select your plan
-            </h1>
-            <p className={styles.desc}>
-                You have the option of monthly or yearly billing.
-            </p>
-            <div className={styles.options} onClick={handleOptions}>
-                <img src={images["arcade"]} className={styles.optionsImage}/>
-                <div>
-                    <h2 className={styles.optionsTitle}>
-                        Arcade
-                    </h2>
-                    <p className={styles.optionsPrice}>
-                        {billing == "monthly"? "$9/mo" : "$90/yr"}  
-                    </p>   
-                    {billing == "yearly" ? <p className={styles.freeMonths}>
-                        2 months free
-                    </p> : <></>}                   
+            <div className={styles.grid}>
+                <h1 className={styles.title}>
+                    Select your plan
+                </h1>
+                <p className={styles.desc}>
+                    You have the option of monthly or yearly billing.
+                </p>
+                <div className={styles.options} onClick={handleOptions}>
+                    <img src={images["arcade"]} className={styles.optionsImage}/>
+                    <div>
+                        <h2 className={styles.optionsTitle}>
+                            Arcade
+                        </h2>
+                        <p className={styles.optionsPrice}>
+                            {billing == "monthly"? "$9/mo" : "$90/yr"}  
+                        </p>   
+                        {billing == "yearly" ? <p className={styles.freeMonths}>
+                            2 months free
+                        </p> : <></>}                   
+                    </div>
                 </div>
-            </div>
-            <div className={styles.options} onClick={handleOptions}> 
-                <img src={images["advanced"]} className={styles.optionsImage}/>
-                <div>
-                    <h2 className={styles.optionsTitle}>
-                        Advanced
-                    </h2>
-                    <p className={styles.optionsPrice}>
-                        {billing == "monthly" ? "$12/mo" : "$120/yr"}  
-                    </p> 
-                   {billing == "yearly" ? <p className={styles.freeMonths}>
-                        2 months free
-                    </p> : <></>}                       
-                </div>
-            </div>
-            <div className={styles.options} onClick={handleOptions}>
-                <img src={images["pro"]} className={styles.optionsImage}/>
-                <div>
-                    <h2 className={styles.optionsTitle}>
-                        Pro
-                    </h2>
-                    <p className={styles.optionsPrice}>
-                        {billing == "monthly" ? "$15/mo" : "$150/yr"}  
-                    </p>  
+                <div className={styles.options} onClick={handleOptions}> 
+                    <img src={images["advanced"]} className={styles.optionsImage}/>
+                    <div>
+                        <h2 className={styles.optionsTitle}>
+                            Advanced
+                        </h2>
+                        <p className={styles.optionsPrice}>
+                            {billing == "monthly" ? "$12/mo" : "$120/yr"}  
+                        </p> 
                     {billing == "yearly" ? <p className={styles.freeMonths}>
-                        2 months free
-                    </p> : <></>}                         
+                            2 months free
+                        </p> : <></>}                       
+                    </div>
+                </div>
+                <div className={styles.options} onClick={handleOptions}>
+                    <img src={images["pro"]} className={styles.optionsImage}/>
+                    <div>
+                        <h2 className={styles.optionsTitle}>
+                            Pro
+                        </h2>
+                        <p className={styles.optionsPrice}>
+                            {billing == "monthly" ? "$15/mo" : "$150/yr"}  
+                        </p>  
+                        {billing == "yearly" ? <p className={styles.freeMonths}>
+                            2 months free
+                        </p> : <></>}                         
+                    </div>
+
+                </div>
+                <div className={styles.switchContainer}>
+                    <p className={styles.monthly} id={styles.currentPlan} ref={monthly}>
+                        Monthly
+                    </p>
+                    <div className={styles.switch} onClick={handleSwitch} ref={switchRef}>
+                        <div className={styles.head}></div>
+                    </div>
+                    <p className={styles.yearly} ref={yearly}>
+                        Yearly
+                    </p>
                 </div>
 
             </div>
-            <div className={styles.switchContainer}>
-                <p className={styles.monthly} id={styles.currentPlan} ref={monthly}>
-                    Monthly
-                </p>
-                <div className={styles.switch} onClick={handleSwitch} ref={switchRef}>
-                    <div className={styles.head}></div>
-                </div>
-                <p className={styles.yearly} ref={yearly}>
-                    Yearly
-                </p>
+
+            <div className={styles.buttons}>
+                <button className={styles.goBack}>Go Back</button>
+                <button className={styles.nextStep}>Next Step</button>
             </div>
         </section>
     )
