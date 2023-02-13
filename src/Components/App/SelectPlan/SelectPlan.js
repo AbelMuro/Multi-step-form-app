2a47b83780df04e3950e1d63de2451191b1511a6
abelmuro <abelmuro93@gmail.com>
2023-02-13 13:48:01 -0800

    commit

--- src/Components/App/SelectPlan/SelectPlan.js
+++ src/Components/App/SelectPlan/SelectPlan.js
@@ -1,6 +1,7 @@
 import React, {useRef, useEffect, useState, useLayoutEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
+import {useMediaQuery} from '@mui/material';
 import styles from "./styles.module.css";
 import images from './images';
 import prices from './../Prices';
@@ -9,8 +10,12 @@ import prices from './../Prices';
 function SelectPlan() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const mobile = useMediaQuery('(max-width: 720px)');
     const previousPlan = useSelector(state => state.plan)       //in case the user clicks on 'go back' button, then we can display the previously selected options
     const grid = useRef();
+    const arcadeIcon = useRef();
+    const advancedIcon = useRef();
+    const proIcon = useRef();
     const switchRef = useRef();
     const monthly = useRef();
     const yearly = useRef();
@@ -55,17 +60,24 @@ function SelectPlan() {
    }, [])
 
    //this useEffect will re-style the switch element 
-   useLayoutEffect(() => {
-        const head = switchRef.current.querySelector("." + styles.head);
+   useEffect(() => {
         if(billing == "Yearly"){
+            grid.current.style.height = mobile ? "566px" : "";                      //we will only change the height of this element when the viewport is in mobile
+            arcadeIcon.current.style.alignSelf = mobile ? "flex-start" : "";
+            advancedIcon.current.style.alignSelf = mobile ? "flex-start" : "";
+            proIcon.current.style.alignSelf = mobile ? "flex-start" : "";
             monthly.current.id = "";
             yearly.current.id = styles.currentPlan;   
-            head.style.left = "20px";
+            switchRef.current.style.left = "20px";
         }
         else if(billing == "Monthly"){
+            grid.current.style.height = "";
+            arcadeIcon.current.style.alignSelf = "";
+            advancedIcon.current.style.alignSelf = "";
+            proIcon.current.style.alignSelf = "";
             monthly.current.id = styles.currentPlan;
             yearly.current.id = "";
-            head.style.left = "";
+            switchRef.current.style.left = "";
         }
    }, [billing])
 
@@ -93,14 +105,17 @@ function SelectPlan() {
     return(
         <section className={styles.yourInfo}>
             <div className={styles.grid} ref={grid}>
-                <h1 className={styles.title}>
-                    Select your plan
-                </h1>
-                <p className={styles.desc}>
-                    You have the option of monthly or yearly billing.
-                </p>
+                <div className={styles.header}>
+                    <h1 className={styles.title}>
+                        Select your plan
+                    </h1>
+                    <p className={styles.desc}>
+                        You have the option of monthly or yearly billing.
+                    </p>                    
+                </div>
+
                 <div className={styles.options} onClick={handlePlanChange} id={styles.optionChoosen} data-plan="Arcade">
-                    <img src={images["arcade"]} className={styles.optionsImage} />
+                    <img src={images["arcade"]} className={styles.optionsImage} ref={arcadeIcon}/>
                     <div>
                         <h2 className={styles.optionsTitle}>
                             Arcade
@@ -114,7 +129,7 @@ function SelectPlan() {
                     </div>
                 </div>
                 <div className={styles.options} onClick={handlePlanChange} data-plan="Advanced"> 
-                    <img src={images["advanced"]} className={styles.optionsImage}/>
+                    <img src={images["advanced"]} className={styles.optionsImage} ref={advancedIcon}/>
                     <div>
                         <h2 className={styles.optionsTitle}>
                             Advanced
@@ -128,7 +143,7 @@ function SelectPlan() {
                     </div>
                 </div>
                 <div className={styles.options} onClick={handlePlanChange} data-plan="Pro">
-                    <img src={images["pro"]} className={styles.optionsImage}/>
+                    <img src={images["pro"]} className={styles.optionsImage} ref={proIcon}/>
                     <div>
                         <h2 className={styles.optionsTitle}>
                             Pro
@@ -146,8 +161,8 @@ function SelectPlan() {
                     <p className={styles.monthly} id={styles.currentPlan} ref={monthly}>
                         Monthly
                     </p>
-                    <div className={styles.switch} onClick={handleSwitch} ref={switchRef}>
-                        <div className={styles.head}></div>
+                    <div className={styles.switch} onClick={handleSwitch} >
+                        <div className={styles.head} ref={switchRef}></div>
                     </div>
                     <p className={styles.yearly} ref={yearly}>
                         Yearly
